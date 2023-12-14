052ddbe68f7e4c1a2b21dd8718a10ed55a2ae466
Girisha DB <SESA740752@se.com>
2023-12-14 23:05:15 +0530

    test123

--- src/Terraform/data.tf
+++ src/Terraform/data.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "lambda_policy" {
       "logs:PutLogEvents",
     ]
 
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.parameter_replication.function_name}:*"]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${join("-", [var.platform_name, var.account_name, var.function_name, "policy", var.env])}:*"]
   }
 
   statement {
