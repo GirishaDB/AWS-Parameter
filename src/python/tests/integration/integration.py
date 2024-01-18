52825b72e69d8b17adb5664c7880370c0f007f1f
Girisha DB <SESA740752@se.com>
2024-01-18 14:03:57 +0530

    test

--- src/python/tests/integration/integration.py
+++ src/python/tests/integration/integration.py
@@ -12,59 +12,76 @@ def create_parameters():
     # Create parameters
     ssm_client.put_parameter(Name='/sample/parameter1',
                              Value='value1', Type='String', Overwrite=True)
-
     print("Parameters Created")
 
 
 def validate_event_bridge_create_capture():
-    time.sleep(50)
+    start_time = time.time()
     logs_client = boto3.client(
         'logs', region_name=os.environ.get('SOURCE_REGION'))
-    log_group_name = f'/aws/events/se-intelds-{os.environ.get("EDH_ACC_NAME_SHORT")}-euwe1-parameter-replication-test-{os.environ.get("ENV")}'
+    log_group_name = f'/aws/events/se-intelds-{os.environ.get(
+        "EDH_ACC_NAME_SHORT")}-euwe1-parameter-replication-logs-{os.environ.get("ENV")}'
     parameter_name = '/sample/parameter1'
 
-    response = logs_client.describe_log_streams(
-        logGroupName=log_group_name,
-        descending=True,
-        limit=50
-    )
+    try:
+        while time.time() - start_time < 70:
+            response = logs_client.describe_log_streams(
+                logGroupName=log_group_name,
+                descending=True,
+                limit=50
+            )
 
-    log_streams = response['logStreams']
-    latest_log_stream_name = max(log_streams, key=lambda x: x['lastIngestionTime'])[
-        'logStreamName']
+            log_streams = response['logStreams']
+            latest_log_stream_name = max(log_streams, key=lambda x: x['lastIngestionTime'])[
+                'logStreamName']
 
-    response = logs_client.get_log_events(
-        logGroupName=log_group_name,
-        logStreamName=latest_log_stream_name,
-        limit=1,
-        startFromHead=True
-    )
-    latest_event = response['events'][0]
-    event_message = json.loads(latest_event['message'])
+            response = logs_client.get_log_events(
+                logGroupName=log_group_name,
+                logStreamName=latest_log_stream_name,
+                limit=1,
+                startFromHead=True
+            )
+            if 'events' in response and response['events']:
+                latest_event = response['events'][0]
+                event_message = json.loads(latest_event['message'])
 
-    if event_message['detail']['name'] == parameter_name and event_message['detail']['operation'] == "Create":
-        print("Validation successful: Event bridge captured 'Create' event")
-    else:
+                if event_message['detail']['name'] == parameter_name and event_message['detail']['operation'] == "Create":
+                    print("Validation successful: Event bridge captured 'Create' event")
+                    return
+            time.sleep(2)
+
         print("Validation failed: Event bridge failed to capture 'Create' event")
+
+    except Exception as e:
+        print(f"Error during validation: {e}")
+        # Cleanup by deleting parameters if script fails
+        delete_parameters()
+        raise
 
 
 def validate_secondary_region_create_parameter():
-
+    start_time = time.time()
     ssm_client_secondary = boto3.client(
         'ssm', region_name=os.environ.get('DEST_REGION'))
 
-    try:
-        parameter1_secondary = ssm_client_secondary.get_parameter(
-            Name='/sample/parameter1')
+    parameter_name = '/sample/parameter1'
 
-        if 'Value' in parameter1_secondary['Parameter']:
-            assert parameter1_secondary['Parameter']['Value'] == 'value1'
-            print("Validation successful: Parameter found in the secondary region.")
-        else:
-            print("Validation failed: Parameter not found in the secondary region.")
+    while time.time() - start_time < 70:
+        try:
+            parameter1_secondary = ssm_client_secondary.get_parameter(
+                Name=parameter_name)
 
-    except ssm_client_secondary.exceptions.ParameterNotFound:
-        print("Validation failed: Parameter not found in the secondary region.")
+            if 'Value' in parameter1_secondary['Parameter']:
+                assert parameter1_secondary['Parameter']['Value'] == 'value1'
+                print("Validation successful: Parameter found in the secondary region.")
+                return
+
+        except ssm_client_secondary.exceptions.ParameterNotFound:
+            pass
+
+        time.sleep(2)
+
+    print("Validation failed: Parameter not found in the secondary region.")
 
 
 def update_parameters():
@@ -79,54 +96,72 @@ def update_parameters():
 
 
 def validate_event_bridge_update_capture():
-    time.sleep(50)
+    start_time = time.time()
     logs_client = boto3.client(
         'logs', region_name=os.environ.get('SOURCE_REGION'))
-    log_group_name = f'/aws/events/se-intelds-{os.environ.get("EDH_ACC_NAME_SHORT")}-euwe1-parameter-replication-logs-{os.environ.get("ENV")}'
+    log_group_name = f'/aws/events/se-intelds-{os.environ.get(
+        "EDH_ACC_NAME_SHORT")}-euwe1-parameter-replication-logs-{os.environ.get("ENV")}'
     parameter_name = '/sample/parameter1'
 
-    response = logs_client.describe_log_streams(
-        logGroupName=log_group_name,
-        descending=True,
-        limit=50
-    )
+    try:
+        while time.time() - start_time < 70:
+            response = logs_client.describe_log_streams(
+                logGroupName=log_group_name,
+                descending=True,
+                limit=50
+            )
 
-    log_streams = response['logStreams']
-    latest_log_stream_name = max(log_streams, key=lambda x: x['lastIngestionTime'])[
-        'logStreamName']
+            log_streams = response['logStreams']
+            latest_log_stream_name = max(log_streams, key=lambda x: x['lastIngestionTime'])[
+                'logStreamName']
 
-    response = logs_client.get_log_events(
-        logGroupName=log_group_name,
-        logStreamName=latest_log_stream_name,
-        limit=1,
-        startFromHead=True
-    )
-    latest_event = response['events'][0]
-    event_message = json.loads(latest_event['message'])
+            response = logs_client.get_log_events(
+                logGroupName=log_group_name,
+                logStreamName=latest_log_stream_name,
+                limit=1,
+                startFromHead=True
+            )
+            if 'events' in response and response['events']:
+                latest_event = response['events'][0]
+                event_message = json.loads(latest_event['message'])
 
-    if event_message['detail']['name'] == parameter_name and event_message['detail']['operation'] == "Update":
-        print("Validation successful: Event bridge captured 'Update' event")
-    else:
+                if event_message['detail']['name'] == parameter_name and event_message['detail']['operation'] == "Update":
+                    print("Validation successful: Event bridge captured 'Update' event")
+                    return
+            time.sleep(2)
+
         print("Validation failed: Event bridge failed to capture 'Update' event")
+
+    except Exception as e:
+        print(f"Error during validation: {e}")
+        # Cleanup by deleting parameters if script fails
+        delete_parameters()
+        raise
 
 
 def validate_secondary_region_update_parameter():
-
+    start_time = time.time()
     ssm_client_secondary = boto3.client(
         'ssm', region_name=os.environ.get('DEST_REGION'))
 
-    try:
-        parameter1_secondary = ssm_client_secondary.get_parameter(
-            Name='/sample/parameter1')
+    parameter_name = '/sample/parameter1'
 
-        if 'Value' in parameter1_secondary['Parameter']:
-            assert parameter1_secondary['Parameter']['Value'] == 'updated_value1'
-            print("Validation successful: Parameter found in the secondary region.")
-        else:
-            print("Validation failed: Parameter not found in the secondary region.")
+    while time.time() - start_time < 70:
+        try:
+            parameter1_secondary = ssm_client_secondary.get_parameter(
+                Name=parameter_name)
 
-    except ssm_client_secondary.exceptions.ParameterNotFound:
-        print("Validation failed: Parameter not found in the secondary region.")
+            if 'Value' in parameter1_secondary['Parameter']:
+                assert parameter1_secondary['Parameter']['Value'] == 'updated_value1'
+                print("Validation successful: Parameter found in the secondary region.")
+                return
+
+        except ssm_client_secondary.exceptions.ParameterNotFound:
+            pass
+
+        time.sleep(2)
+
+    print("Validation failed: Parameter not found in the secondary region.")
 
 
 def delete_parameters():
@@ -140,52 +175,62 @@ def delete_parameters():
 
 
 def validate_event_bridge_delete_capture():
-    time.sleep(50)
+    start_time = time.time()
     logs_client = boto3.client(
         'logs', region_name=os.environ.get('SOURCE_REGION'))
-    log_group_name = f'/aws/events/se-intelds-{os.environ.get("EDH_ACC_NAME_SHORT")}-euwe1-parameter-replication-logs-{os.environ.get("ENV")}'
+    log_group_name = f'/aws/events/se-intelds-{os.environ.get(
+        "EDH_ACC_NAME_SHORT")}-euwe1-parameter-replication-logs-{os.environ.get("ENV")}'
     parameter_name = '/sample/parameter1'
 
-    response = logs_client.describe_log_streams(
-        logGroupName=log_group_name,
-        descending=True,
-        limit=50
-    )
+    while time.time() - start_time < 70:
+        response = logs_client.describe_log_streams(
+            logGroupName=log_group_name,
+            descending=True,
+            limit=50
+        )
 
-    log_streams = response['logStreams']
-    latest_log_stream_name = max(log_streams, key=lambda x: x['lastIngestionTime'])[
-        'logStreamName']
+        log_streams = response['logStreams']
+        latest_log_stream_name = max(log_streams, key=lambda x: x['lastIngestionTime'])[
+            'logStreamName']
 
-    response = logs_client.get_log_events(
-        logGroupName=log_group_name,
-        logStreamName=latest_log_stream_name,
-        limit=1,
-        startFromHead=True
-    )
-    latest_event = response['events'][0]
-    event_message = json.loads(latest_event['message'])
+        response = logs_client.get_log_events(
+            logGroupName=log_group_name,
+            logStreamName=latest_log_stream_name,
+            limit=1,
+            startFromHead=True
+        )
+        if 'events' in response and response['events']:
+            latest_event = response['events'][0]
+            event_message = json.loads(latest_event['message'])
 
-    if event_message['detail']['name'] == parameter_name and event_message['detail']['operation'] == "Delete":
-        print("Validation successful: Event bridge captured 'Delete' event")
-    else:
-        print("Validation failed: Event bridge failed to capture 'Delete' event")
+            if event_message['detail']['name'] == parameter_name and event_message['detail']['operation'] == "Delete":
+                print("Validation successful: Event bridge captured 'Delete' event")
+                return
+        time.sleep(2)
+
+    print("Validation failed: Event bridge failed to capture 'Delete' event")
 
 
 def validate_secondary_region_delete_parameter():
+    start_time = time.time()
     ssm_client_secondary = boto3.client(
         'ssm', region_name=os.environ.get('DEST_REGION'))
 
-    try:
-        # Attempt to get the parameter in the secondary region after delete
-        parameter1_secondary = ssm_client_secondary.get_parameter(
-            Name='/sample/parameter1')
+    parameter_name = '/sample/parameter1'
 
-        # If the parameter is found, validation fails
-        print(
-            "Validation failed: Parameter still found in the secondary region after delete.")
+    while time.time() - start_time < 70:
+        try:
+            parameter1_secondary = ssm_client_secondary.get_parameter(
+                Name=parameter_name)
 
-    except ssm_client_secondary.exceptions.ParameterNotFound:
-        print("Validation successful: Parameter not found in the secondary region after delete.")
+            time.sleep(2)  # Adjust the sleep duration based on your needs
+
+        except ssm_client_secondary.exceptions.ParameterNotFound:
+            print(
+                "Validation successful: Parameter not found in the secondary region after delete.")
+            return
+
+    print("Validation failed: Parameter still found in the secondary region after delete.")
 
 
 # Validating Create Operation
