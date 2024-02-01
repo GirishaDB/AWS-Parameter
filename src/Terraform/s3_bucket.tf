b35f3849cd8145aae56cea6bd648ee3c756a0b9f
Girisha DB <SESA740752@se.com>
2024-02-01 18:22:44 +0530

    terraform-scan

---
+++ src/Terraform/s3_bucket.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  tags   = merge({ "resourcename" = "${local.name}" }, local.tags)
+}
+
+locals {
+  name = "${var.project}-${var.prefix}"
+  tags = {
+    project   = var.project
+    createdon = timestamp()
+  }
+}
+
+
+# Tags
+variable "project" {
+  description = "name of project"
+  type        = string
+  default     = "demo"
+}
+
+variable "prefix" {
+  description = "prefix name"
+  type        = string
+  default     = "pref"
+}
+
+# Amazon S3
+variable "bucket_name" {
+  description = "name of s3 bucket"
+  type        = string
+  default     = null
+}
