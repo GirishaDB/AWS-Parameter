254160190fe28bed197b2e656c45a276592c19df
Girisha DB <SESA740752@se.com>
2024-02-05 11:10:50 +0530

    test sonar

--- src/Terraform/security_groups.tf
+++ src/Terraform/security_groups.tf
@@ -1,64 +1,64 @@
 
-resource "aws_security_group" "https-sg" {
-  vpc_id      = local.vpc_id
-  name        = local.https_sg
-  description = "Security Group for endpoints - https traffic"
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allowing https traffic for specific subnet IPs ranges"
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allowing All Outbound Traffic"
-  }
+# resource "aws_security_group" "https-sg" {
+#   vpc_id      = local.vpc_id
+#   name        = local.https_sg
+#   description = "Security Group for endpoints - https traffic"
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allowing https traffic for specific subnet IPs ranges"
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allowing All Outbound Traffic"
+#   }
 
-  tags = {
-    "anycompany:cost-center" = "Accounting"
-  }
-}
-
-
-resource "aws_security_group" "https-test" {
-  vpc_id      = local.vpc_id
-  name        = local.https_sg
-  description = "Security Group for endpoints - https traffic"
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allowing https traffic for specific subnet IPs ranges"
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allowing All Outbound Traffic"
-  }
-
-  tags = {
-    "anycompany:cost-center" = "Accounting"
-  }
-}
+#   tags = {
+#     "anycompany:cost-center" = "Accounting"
+#   }
+# }
 
 
-resource "aws_security_group" "noncompliant" {
-  name        = "allow_ssh_noncompliant"
-  description = "allow_ssh_noncompliant"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "https-test" {
+#   vpc_id      = local.vpc_id
+#   name        = local.https_sg
+#   description = "Security Group for endpoints - https traffic"
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allowing https traffic for specific subnet IPs ranges"
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allowing All Outbound Traffic"
+#   }
 
-  ingress {
-    description = "SSH rule"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   tags = {
+#     "anycompany:cost-center" = "Accounting"
+#   }
+# }
+
+
+# resource "aws_security_group" "noncompliant" {
+#   name        = "allow_ssh_noncompliant"
+#   description = "allow_ssh_noncompliant"
+#   vpc_id      = aws_vpc.main.id
+
+#   ingress {
+#     description = "SSH rule"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
